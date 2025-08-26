3ee6c57634b2ab02ce4873c1a29e91b97a23fd12
XYWINGS <chalithbandara99@gmail.com>
2025-08-26 14:15:53 +0530

    feat: update player schema with additional fields and integrate with room schema

--- models/Player.js
+++ models/Player.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
-
 const playerSchema = new mongoose.Schema({
   name: { type: String, required: true },
+  isHost: { type: Boolean, default: false },
   role: {
     type: String,
     enum: ["villager", "demon", "demonLeader", "inspector", "doctor", "vampire"],
+    default: "villager",
   },
   alive: { type: Boolean, default: true },
-  roomId: { type: mongoose.Schema.Types.ObjectId, ref: "Room" },
+  killedBy: { type: String, enum: ["demon", "vampire", "villagers"], default: null },
+  votes: { type: Number, default: 0 },
+  roomId: { type: mongoose.Schema.Types.ObjectId, ref: "Room", required: true },
 });
 
 module.exports = mongoose.model("Player", playerSchema);
--- models/Room.js
+++ models/Room.js
@@ -1,15 +1,6 @@
 const mongoose = require("mongoose");
-
-const playerSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  isHost: { type: Boolean, default: false },
-  role: {
-    type: String,
-    enum: ["villager", "demon", "demonLeader", "inspector", "doctor", "vampire"],
-    default: "villager",
-  },
-  alive: { type: Boolean, default: true },
-});
+const Player = require("../models/Player");
+const playerSchema = Player.schema;
 
 const roomSchema = new mongoose.Schema({
   code: { type: String, required: true, unique: true },
